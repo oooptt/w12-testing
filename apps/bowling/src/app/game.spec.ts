5d609fcba1c49355b0ed351561d5d684192c26ac
handsome baldfattie <68840795+oooptt@users.noreply.github.com>
2022-05-04 21:30:12 +0800

    update

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -22,9 +22,20 @@ describe('Game', () => {
     expect(game.score).toBe(20);
   });
 
+  // game.spec.ts
+  test('test on one spare', () => {
+    game.roll(5);
+    game.roll(5); // spare
+    game.roll(3);
+    rollMany(17, 0);
+    expect(game.score).toBe(16);
+  });
+
   function rollMany(n: number, pins: number) {
     for (let i = 0; i < n; i++) {
       game.roll(pins);
     }
   }
+
+
 });
--- apps/bowling/src/app/game.ts
+++ apps/bowling/src/app/game.ts
@@ -1,11 +1,26 @@
+// game.ts
 export class Game {
-    private _score = 0;
+    private rolls: number[] = [];
+    private currentRoll = 0;
     roll(pins: number) {
-        this._score += pins;
+      this.rolls[this.currentRoll++] = pins;
     }
-
+  
     get score() {
-        return this._score;
+      let score = 0;
+      let frameIndex = 0;
+      for (let frame = 0; frame < 10; frame++) {
+        if (this.isSpare(frameIndex)) {
+          score += 10 + this.rolls[frameIndex + 2];
+        } else {
+          score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+        }
+        frameIndex += 2;
+      }
+      return score;
     }
-    
+  
+    private isSpare(frameIndex: number) {
+      return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
+    }
   }
