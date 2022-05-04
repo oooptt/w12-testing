f13c693808ef80522658e6871a6afda63dcd4977
handsome baldfattie <68840795+oooptt@users.noreply.github.com>
2022-05-04 20:38:19 +0800

    update

---
+++ apps/bowling/src/app/game.spec.ts
@@ -0,0 +1,30 @@
+// game.spec.ts
+import { Game } from './game';
+
+describe('Game', () => {
+  let game: Game;
+
+  beforeEach(() => {
+    game = new Game();
+  });
+
+  it('should create an instance', () => {
+    expect(game).toBeTruthy();
+  });
+
+  test('all zero', () => {
+    rollMany(20, 0);
+    expect(game.score).toBe(0);
+  });
+
+  test('all one', () => {
+    rollMany(20, 1);
+    expect(game.score).toBe(20);
+  });
+
+  function rollMany(n: number, pins: number) {
+    for (let i = 0; i < n; i++) {
+      game.roll(pins);
+    }
+  }
+});
---
+++ apps/bowling/src/app/game.ts
@@ -0,0 +1,11 @@
+export class Game {
+    private _score = 0;
+    roll(pins: number) {
+        this._score += pins;
+    }
+
+    get score() {
+        return this._score;
+    }
+    
+  }
